a27bc2bfdb0e4949e331bf974c143d7522d32a5d
unknown <cauarios30@gmail.com>
2026-03-11 17:22:22 -0300

    11/03/26

---
+++ main.py
@@ -0,0 +1,123 @@
+import json
+import os
+
+LOG_JSON = os.path.join('log.json') 
+
+title = "////|Calculadora de dois numeros|////"
+
+
+def json_log(a, b, opreracao, resultado): # função para salvar e abrir aquivo json
+    try:
+
+        with open(LOG_JSON, 'w') as json_f:
+
+            log = {
+                "a": a,
+                "b": b,
+                "operacao": opreracao,
+                "resultado": resultado
+            }
+
+            json.dump(log, json_f, indent=4)
+
+        with open(LOG_JSON, 'r') as json_f:
+
+            row = json.load(json_f)
+
+            log = json.dumps(row, indent=4)
+
+            return log
+
+    except Exception as e:
+
+        raise Exception(e)
+
+
+
+def operacao(a, b, row_opr:str): # função para definir qual a operação, e calcular com base na operação selecionada
+
+    opr = row_opr.strip().lower()
+
+    if opr == 'soma':
+
+        result = a + b
+
+        print(f"{a} + {b} = {result}\n")
+
+        log = json_log(a=a, b=b, opreracao=opr, resultado=result)
+
+        print(log)
+
+    elif opr == 'subtracao':
+        
+        result = a - b
+
+        print(f"{a} - {b} = {result}\n")
+
+        log = json_log(a=a, b=b, opreracao=opr, resultado=result)
+
+        print(log)
+
+    elif opr == 'multiplicacao':
+
+        result = a * b
+
+        print(f"{a} x {b} = {result}\n")
+
+        log = json_log(a=a, b=b, opreracao=opr, resultado=result)
+
+        print(log)
+
+    elif opr == 'divisao':
+
+        result = a / b
+
+        print(f"{a} ÷ {b} = {result}\n")
+
+        log = json_log(a=a, b=b, opreracao=opr, resultado=result)
+
+        print(log)
+    
+    else:
+        
+        result == None
+
+
+    return result
+
+
+
+while True:
+
+    print(title)
+
+    try:
+
+        a = int(input("Digite o primeiro número (para encerrar o programa pressione <ctrl + c>)\n> "))
+
+        b = int(input("Digite o segundo número\n> "))
+
+    except ValueError as e:
+
+        print("Valor invalido! Digite somente numeros inteiros!\nTente novamente.\n")
+
+        continue
+
+
+    opr = input("Defina a operação ('soma', 'subtracao', 'multiplicacao', 'divisao')\n> ")
+
+    try:
+        resultado = operacao(a=a, b=b, row_opr=opr)
+
+        if resultado == None:
+            print("Operação invalida!\nTente novamente.\n")
+
+            continue
+
+    except Exception as e:
+
+        print(f"Error: {e}")
+
+        continue
+
+    input("Pressione <enter> para calcular outro número. Ou pressione <ctrl + c> para encerrar.\n")
